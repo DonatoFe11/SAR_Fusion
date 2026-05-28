57ffe8844fa111dd309f44ca04f155aa3b2b2b1d
DonatoFe11 <donatofesta.df@gmail.com>
2026-05-28 21:20:59 +0200

    Testing results on run executed on the same seed

--- sarfusion/experiment/run.py
+++ sarfusion/experiment/run.py
@@ -83,6 +83,8 @@ class Run:
 
     def init(self, params: dict):
         set_seed(params["seed"])
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = True
         self.seg_trainer = None
         logger.info("Parameters: ")
         write_yaml(params, file=sys.stdout)
@@ -471,6 +473,8 @@ class Run:
     ):
         if epoch > 0:
             set_seed(self.params["seed"] + epoch)
+            torch.backends.cudnn.deterministic = True
+            torch.backends.cudnn.benchmark = True
             logger.info(f"Setting seed to {self.params['seed'] + epoch}")
         self.tracker.log_metric("start_epoch", epoch)
         self.model.train()
